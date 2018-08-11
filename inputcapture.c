227a95ca663adecf85c004d50b08f84e96b5d574
ardeleanasm <ardeleanasm@gmail.com>
2018-08-11 21:38:28 +0300

    allocation in specific function and deallocation in macros

--- inputcapture.c
+++ inputcapture.c
@@ -28,6 +28,21 @@
 #define VERSION_MINOR_NUMBER 0x00u
 #define VERSION_PATCH_NUMBER 0x00u
 
+
+#define FREE_DEVICE() \
+  kfree(icdev_Ptr);	   \
+  icdev_Ptr = NULL
+
+#define UNREGISTER_REGION() unregister_chrdev_region(icdev_no,1)
+
+#define DESTROY_CLASS() \
+  class_destroy(icdev_class_Ptr);		\
+  icdev_class_Ptr = NULL
+
+#define DESTROY_DEVICE() \
+  device_destroy(icdev_class_Ptr,MKDEV(MAJOR(icdev_no),0));	\
+  cdev_del(&(icdev_Ptr->cdev))
+
 struct ic_device {
   struct cdev cdev;
   struct device *device_Ptr;
@@ -80,67 +95,87 @@ static irq_handler_t irq_event_handler(int irq, void *dev_id, struct pt_regs *re
 
 
 
+static s8 alloc_device(void)
+{
+  icdev_Ptr=kmalloc(sizeof(struct ic_device),GFP_KERNEL);
+  if (icdev_Ptr == NULL) {
+    printk(KERN_WARNING "%s:Failed to alloc memory for ic_device\n",DEVICE_NAME);
+    return -1;
+  }
+  memset(icdev_Ptr,0,sizeof(struct ic_device));
+  if (alloc_chrdev_region(&icdev_no,0,1,DEVICE_NAME) < 0) {
+    printk(KERN_WARNING "%s:Could not register\n",DEVICE_NAME);
+    FREE_DEVICE();
+    return -1;
+  }
+  return 0;
+}
 
 
+static s8 init_class(void)
+{
+  icdev_class_Ptr=class_create(THIS_MODULE,DEVICE_CLASS_NAME);
+  if (IS_ERR(icdev_class_Ptr)) {
+    printk(KERN_WARNING "%s:Could not create class\n",DEVICE_NAME);
+    UNREGISTER_REGION();
+    FREE_DEVICE();
+    return -1;
+  }
+  return 0;
+}
+
+static s8 init_device_registration(void)
+{
+  cdev_init(&icdev_Ptr->cdev,&fops);
+  icdev_Ptr->cdev.owner = THIS_MODULE;
+  if (cdev_add(&(icdev_Ptr->cdev),icdev_no,1) != 0) {
+    printk(KERN_WARNING "%s:Could not add device\n",DEVICE_NAME);
+    DESTROY_CLASS();
+    UNREGISTER_REGION();
+    FREE_DEVICE();
+    return -1;
+  }
+
+  icdev_Ptr->device_Ptr = device_create(icdev_class_Ptr,NULL,MKDEV(MAJOR(icdev_no),0),NULL,DEVICE_PROCESS,0);
+  if (IS_ERR(icdev_Ptr->device_Ptr)) {
+    printk(KERN_WARNING "%s:Could not create device\n",DEVICE_NAME);
+    DESTROY_DEVICE();
+    DESTROY_CLASS();
+    UNREGISTER_REGION();
+    FREE_DEVICE();
+    return -1;
+  }
+  return 0;
+
+}
 
 /*
  * Driver Initialization
  */
 static int __init ic_init(void)
 {
+  s8 init_error = 0;
+  init_error=alloc_device();
+  if (init_error == -1)
+    goto ic_init_error;
 
-  icdev_Ptr=kmalloc(sizeof(struct ic_device),GFP_KERNEL);
-  if (icdev_Ptr == NULL) {
-    printk(KERN_WARNING "%s:Failed to alloc memory for ic_device\n",DEVICE_NAME);
-    goto failed_allocation;
-  }
-  memset(icdev_Ptr,0,sizeof(struct ic_device));
-  if (alloc_chrdev_region(&icdev_no,0,1,DEVICE_NAME) < 0) {
-    printk(KERN_WARNING "%s:Could not register\n",DEVICE_NAME);
-    goto failed_registration;
-  }
+  init_error = init_class();
+  if (init_error == -1)
+    goto ic_init_error;
+ 
+  init_error = init_device_registration();
+  if (init_error == -1)
+    goto ic_init_error;
 
-  icdev_class_Ptr=class_create(THIS_MODULE,DEVICE_CLASS_NAME);
-  if (IS_ERR(icdev_class_Ptr)) {
-    printk(KERN_WARNING "%s:Could not create class\n",DEVICE_NAME);
-    goto failed_class_creation;
-  }
-  
-  cdev_init(&icdev_Ptr->cdev,&fops);
-  icdev_Ptr->cdev.owner = THIS_MODULE;
-  if (cdev_add(&(icdev_Ptr->cdev),icdev_no,1) != 0) {
-    printk(KERN_WARNING "%s:Could not add device\n",DEVICE_NAME);
-    goto failed_adding_device;
-  }
-
-  icdev_Ptr->device_Ptr = device_create(icdev_class_Ptr,NULL,MKDEV(MAJOR(icdev_no),0),NULL,DEVICE_PROCESS,0);
-  if (IS_ERR(icdev_Ptr->device_Ptr)) {
-    printk(KERN_WARNING "%s:Could not create device\n",DEVICE_NAME);
-    goto failed_device_creation;
-  }
-
+ 
   mutex_init(&(icdev_Ptr->io_mutex));
   printk(KERN_INFO "%s:Registered device with (%d,%d)\n",DEVICE_NAME,MAJOR(icdev_no),MINOR(icdev_no));
   printk(KERN_INFO "Driver %s loaded. Version %2x %2x %2x",DEVICE_NAME,
 	 VERSION_MAJOR_NUMBER,VERSION_MINOR_NUMBER,VERSION_PATCH_NUMBER);
   return 0;
   
- failed_device_creation:
-  device_destroy(icdev_class_Ptr,MKDEV(MAJOR(icdev_no),0));
-  cdev_del(&(icdev_Ptr->cdev));
   
- failed_adding_device:
-  class_destroy(icdev_class_Ptr);
-  icdev_class_Ptr = NULL;
-  
- failed_class_creation:
-  unregister_chrdev_region(icdev_no,1);
-  
- failed_registration:
-  kfree(icdev_Ptr);
-  icdev_Ptr = NULL;
-  
- failed_allocation:
+  ic_init_error:
   return -EBUSY;
   
 }
@@ -149,15 +184,12 @@ static void __exit ic_exit(void)
 {
   printk(KERN_INFO "%s:Unregister...:(",DEVICE_NAME);
   if (icdev_Ptr != NULL) {
-    device_destroy(icdev_class_Ptr,MKDEV(MAJOR(icdev_no),0));
-    cdev_del(&(icdev_Ptr->cdev));
-    kfree(icdev_Ptr);
-    icdev_Ptr = NULL;
+    DESTROY_DEVICE();
+    FREE_DEVICE();
   }
   unregister_chrdev_region(icdev_no,1);
   if (icdev_class_Ptr != NULL) {
-    class_destroy(icdev_class_Ptr);
-    icdev_class_Ptr = NULL;
+    DESTROY_CLASS();
   }
   printk(KERN_INFO "Driver %s unloaded.",DEVICE_NAME);
   
