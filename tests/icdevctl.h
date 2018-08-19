4510afa5c1fb5174c03e50736a903d794a7ebb50
ardeleanasm <ardeleanasm@gmail.com>
2018-08-19 17:56:44 +0300

    error numbers ioctl

--- tests/icdevctl.h
+++ tests/icdevctl.h
@@ -2,15 +2,14 @@
 #define ICDEVCTL_H_
 
 #define _IOCTL_MAGIC 'K'
-#define IOCICUP _IOW(_IOCTL_MAGIC,1,uint16_t) /* register pin */
-#define IOCICDW _IOW(_IOCTL_MAGIC,2,uint16_t) /* unregister ping */
+#define IOCICUP _IOW(_IOCTL_MAGIC,1,u16) /* register pin */
+#define IOCICDW _IOW(_IOCTL_MAGIC,2,u16) /* unregister ping */
 #define IOCICFE _IO(_IOCTL_MAGIC,3) /*Capture timer value on every falling edge*/
 #define IOCICRE _IO(_IOCTL_MAGIC,4) /*Capture timer value on every rising edge*/
 #define IOCICRF _IO(_IOCTL_MAGIC,5) /*Capture timer value on rising and falling*/
 
+#define ICEBIRQN 0x01
+#define ICEBGPIO 0x02
 
-#define ICDEV_DETECT_RISING_EDGES 0x01u
-#define ICDEV_DETECT_FALLING_EDGES 0x02u
-#define ICDEV_DETECT_BOTH_EDGES 0x03u
 
 #endif
